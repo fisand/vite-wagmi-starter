055dfc6dd7401364e4dade2c62c20d695ea9b5a1
zouhang <1102639563@qq.com>
2022-04-12 22:23:07 +0800

    chore: upgrade deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import Pages from 'vite-plugin-pages'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), Pages()],
+  plugins: [
+    react(),
+    Pages(),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
+  ],
   css: {
     preprocessorOptions: {
       less: {
