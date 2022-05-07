4fc1f251909b558308af5e8257ddb9b4e5e86d1e
zouhang <1102639563@qq.com>
2022-05-07 13:23:38 +0800

    chore: update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,11 +9,11 @@ const Home = () => {
       <p className="text-3xl font-bold underline hover:text-blue-300">Hello Vite + React + Antd Dapp!</p>
       <p>{account && shortenAddress(account)}</p>
       <p className='flex gap-4'>
-        <Button type="primary" onClick={activateBrowserWallet}>
-          connect
+        <Button type="primary" onClick={activateBrowserWallet} className="flex items-center">
+          connect <span className='i-carbon:chart-spiral'></span>
         </Button>
-        <Button type="primary" onClick={deactivate}>
-          disconnect
+        <Button type="primary" onClick={deactivate} className="flex items-center">
+          disconnect <span className='i-carbon:cookie'></span>
         </Button>
       </p>
       <p>
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,24 @@
 import { defineConfig } from 'vite'
 import Pages from 'vite-plugin-pages'
 import react from '@vitejs/plugin-react'
+import { splitVendorChunkPlugin } from 'vite'
 // import legacy from '@vitejs/plugin-legacy'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
 import Unocss from 'unocss/vite'
 import presetWind from '@unocss/preset-wind'
 import presetIcons from '@unocss/preset-icons'
-import { splitVendorChunkPlugin } from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    Icons({
+      compiler: 'jsx',
+      jsx: 'react',
+    }),
     Pages({
       exclude: ['**/![index.tsx]'],
     }),
@@ -19,6 +27,15 @@ export default defineConfig({
         presetWind(),
         presetIcons({
           /* options */
+        }),
+      ],
+    }),
+    AutoImport({
+      imports: ['react'],
+      dts: './src/auto-imports.d.ts',
+      resolvers: [
+        IconsResolver({
+          componentPrefix: 'Icon',
         }),
       ],
     }),
