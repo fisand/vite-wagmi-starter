02ede2b13a54d5f2f9a0684213155d969d273195
zouhang <1102639563@qq.com>
2022-07-18 15:25:03 +0800

    chore: support mode base

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import Unocss from 'unocss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import IconsResolver from 'unplugin-icons/resolver'
 import Icons from 'unplugin-icons/vite'
-import { defineConfig, splitVendorChunkPlugin } from 'vite'
+import { defineConfig, loadEnv, splitVendorChunkPlugin } from 'vite'
 import Checker from 'vite-plugin-checker'
 import EslintPlugin from 'vite-plugin-eslint'
 import Imp from 'vite-plugin-imp'
@@ -16,70 +16,77 @@ import unoConfig from './unocss.config'
 
 const theme = unoConfig.theme!
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@/': `${resolve(__dirname, 'src')}/`,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    base: env.BASE,
+    resolve: {
+      alias: {
+        '@/': `${resolve(__dirname, 'src')}/`,
+      },
     },
-  },
-  plugins: [
-    react(),
-    Checker({ typescript: true }),
-    Icons({
-      compiler: 'jsx',
-      jsx: 'react',
-    }),
-    Pages({
-      exclude: ['**/[A-Z]*.tsx'],
-    }),
-    Unocss(),
-    AutoImport({
-      imports: ['react'],
-      dts: './src/auto-imports.d.ts',
-      resolvers: [
-        IconsResolver({
-          componentPrefix: 'Icon',
-        }),
-      ],
-    }),
-    EslintPlugin(),
-    splitVendorChunkPlugin(),
-    Imp({
-      libList: [
-        {
-          libName: 'antd',
-          style(name) {
-            return `antd/es/${name}/style/index.js`
+    plugins: [
+      react(),
+      Checker({ typescript: true }),
+      Icons({
+        compiler: 'jsx',
+        jsx: 'react',
+      }),
+      Pages({
+        dirs: [{ dir: 'src/pages', baseRoute: env.BASE || '' }],
+        exclude: ['**/[A-Z]*.tsx'],
+        importMode: 'sync',
+      }),
+      Unocss(),
+      AutoImport({
+        imports: ['react'],
+        dts: './src/auto-imports.d.ts',
+        resolvers: [
+          IconsResolver({
+            componentPrefix: 'Icon',
+          }),
+        ],
+      }),
+      EslintPlugin(),
+      splitVendorChunkPlugin(),
+      Imp({
+        libList: [
+          {
+            libName: 'antd',
+            style(name) {
+              return `antd/es/${name}/style/index.js`
+            },
+          },
+        ],
+      }),
+      Analyze(),
+      // legacy({
+      //   targets: ['defaults', 'not IE 11'],
+      // }),
+    ],
+    css: {
+      preprocessorOptions: {
+        less: {
+          javascriptEnabled: true,
+          modifyVars: {
+            '@border-radius-base': '8px',
+            '@border-radius-sm': '4px',
+            '@primary-color': theme.colors.primary,
           },
         },
-      ],
-    }),
-    Analyze(),
-    // legacy({
-    //   targets: ['defaults', 'not IE 11'],
-    // }),
-  ],
-  css: {
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true,
-        modifyVars: {
-          '@border-radius-base': '8px',
-          '@border-radius-sm': '4px',
-          '@primary-color': theme.colors.primary,
+      },
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            'react-vendor': ['react', 'react-router-dom', 'react-dom', 'buffer'],
+            'eth-vendor': ['ethers', 'wagmi'],
+            'antd-vendor': ['antd', 'axios'],
+          },
         },
       },
     },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-router-dom', 'react-dom', 'buffer'],
-          'eth-vendor': ['ethers', 'wagmi'],
-          'antd-vendor': ['antd', 'axios'],
-        },
-      },
-    },
-  },
+  }
 })
