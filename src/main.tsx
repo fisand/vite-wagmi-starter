069ecb9736af88ae9b8e3d3caaf254f3ff8f4535
zouhang <1102639563@qq.com>
2023-03-12 10:17:02 +0800

    chore: support wallet v2 & bitkeep

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,10 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { configureChains, createClient, WagmiConfig } from 'wagmi'
 import { arbitrum, arbitrumGoerli, bsc, bscTestnet, goerli, mainnet, polygon } from 'wagmi/chains'
+import { InjectedConnector } from 'wagmi/connectors/injected'
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
 import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+import { WalletConnectLegacyConnector } from 'wagmi/connectors/walletConnectLegacy'
 import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
 import { publicProvider } from 'wagmi/providers/public'
 
@@ -36,18 +38,23 @@ const client = createClient({
   connectors: [
     new MetaMaskConnector({ chains }),
     new WalletConnectConnector({
+      options: {
+        projectId: 'f18c88f1b8f4a066d3b705c6b13b71a8',
+      },
+    }),
+    new WalletConnectLegacyConnector({
       chains,
       options: {
         qrcode: true,
       },
     }),
-    // new InjectedConnector({
-    //   chains,
-    //   options: {
-    //     name: 'Injected',
-    //     shimDisconnect: true,
-    //   },
-    // }),
+    new InjectedConnector({
+      chains,
+      options: {
+        name: 'BitKeep',
+        getProvider: () => (typeof window !== 'undefined' ? (window as any).bitkeep?.ethereum : undefined),
+      },
+    }),
   ],
   provider,
   webSocketProvider,
