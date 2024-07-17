60aa86689a809bc0f7348651e1b6204de6587cba
zouhang <zouhangsweet@gmail.com>
2024-07-17 18:14:30 +0800

    chore: update logic

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,61 @@
-import { sxzz } from '@sxzz/eslint-config'
 import react from '@eslint-react/eslint-plugin'
+import { sxzz } from '@sxzz/eslint-config'
 import reactHooks from 'eslint-plugin-react-hooks'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 // import importOrder from 'eslint-plugin-import-x'
 
 export default sxzz(
-  {
-    files: ['**/*.{ts,tsx}'],
-    ...react.configs.recommended,
-  },
-  {
-    languageOptions: {
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
+  [
+    {
+      files: ['**/*.{ts,tsx}'],
+      ...react.configs.recommended,
+    },
+    {
+      languageOptions: {
+        parserOptions: {
+          ecmaFeatures: {
+            jsx: true,
+          },
         },
       },
+      files: ['**/*.{ts,tsx}'],
+      plugins: {
+        'react-hooks': reactHooks,
+      },
+      rules: {
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'error',
+      },
     },
-    files: ['**/*.{ts,tsx}'],
-    plugins: {
-      'react-hooks': reactHooks,
+    {
+      plugins: {
+        'simple-import-sort': simpleImportSort,
+      },
+      rules: {
+        'sort-imports': [
+          'error',
+          {
+            ignoreCase: true,
+            ignoreDeclarationSort: true,
+            ignoreMemberSort: true,
+            memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+            allowSeparatedGroups: false,
+          },
+        ],
+        'simple-import-sort/imports': [
+          'error',
+          {
+            groups: [[String.raw`^@?\w`], ['^'], [String.raw`^\.`], [String.raw`^\u0000`]],
+          },
+        ],
+        'simple-import-sort/exports': 'error',
+        'import/order': 'off',
+        'import/first': 'error',
+        'import/newline-after-import': 'error',
+        'import/no-duplicates': 'error',
+      },
     },
-    rules: {
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'error',
-    },
-  },
+  ],
   {
     prettier: true,
     markdown: true,
